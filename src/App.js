f4ea5a46af4224baaabbb93c20690df8497b0f1a
L8Duck <ndkhoa1000@gmail.com>
2023-11-09 15:31:32 +0700

    set base structure for the website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
+import Home from './components/pages/Home'
+import Product from './components/pages/Product'
+import Introduce from './components/pages/Introduce'
+import Order from './components/pages/Order'
+import Login from './components/pages/Login'
+import Register from './components/pages/Register'
+import Contact from './components/pages/Contact'
+
+import Navigation from './components/Navigation'
+import Footer from './components/Footer'
+
+const App = () => {
+  const [Page, setPage] = useState("home")
+  let content; 
+  switch (Page) {
+    case "home":
+      content = <Home/>;
+      break;
+    case "product":
+      content = <Product/>;
+      break;
+    case "introduce":
+      content = <Introduce />;
+      break;
+    case "order":
+      content = <Order/>;
+      break;
+    case "login":
+      content = <Login/>;
+      break;
+    case "register":
+      content = <Register />;
+      break;
+    case "contact":
+      content = <Contact />;
+      break;       
+    default:
+      content = <Home />;
+      break;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation 
+      Page = {Page}
+      onPageChange = {setPage}/>
+      {content}
+      <Footer/>
     </div>
   );
 }
 
-export default App;
+export default App
